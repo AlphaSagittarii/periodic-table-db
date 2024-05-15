0008f4265eb05b692a1517a216f458d4ba39c43c
AlphaSagittarii G <mg.esec@intelligence-airbusds.de>
2024-05-15 11:26:37 +0000

    fix: fixed element.sh

--- element.sh
+++ element.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
+PARSE_RESULT() {
+  getElements=$($PSQL "SELECT * FROM elements e FULL JOIN properties p ON e.atomic_number = p.atomic_number FULL JOIN types t ON p.type_id = t.type_id")
+  echo "$getElements" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_NUMBER BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID BAR TYPE_ID BAR TYPE
+  do
+    case $INPUT in
+    $ATOMIC_NUMBER|$SYMBOL|$NAME) 
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    ;;
+    esac
+  done
+}
+
 INPUT=$1
+# if input is number
 if [[ $INPUT =~ ^[0-9]+$ ]]
-then 
+then
   tryElements=$($PSQL "SELECT * FROM elements WHERE atomic_number = $INPUT")
-  echo "I could not find that element in the database."
-else
-  if [[ $INPUT ]]
+  # if empty
+  if [[ -z $tryElements ]]
   then
-    tryElements=$($PSQL "SELECT * FROM elements WHERE symbol = '$INPUT' OR name = '$INPUT'")
+    echo "I could not find that element in the database."
+  else
+    PARSE_RESULT
+  fi
+elif [[ $INPUT ]]
+  # check db
+then
+  tryElements=$($PSQL "SELECT * FROM elements WHERE symbol = '$INPUT' OR name = '$INPUT'")
+  # if empty
     if [[ -z $tryElements ]]
     then
-    echo "I could not find that element in the database."
+      echo "I could not find that element in the database."
     else
-      getElements=$($PSQL "SELECT * FROM elements e FULL JOIN properties p ON e.atomic_number = p.atomic_number")
-      echo "$getElements" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_NUMBER BAR TYPE BAR ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
-      do
-        case $INPUT in
-          $ATOMIC_NUMBER|$SYMBOL|$NAME) 
-          echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-          ;;
-        esac
-      done
+      PARSE_RESULT
     fi
-  else
-    echo "Please provide an element as an argument."
-  fi
+else
+  echo "Please provide an element as an argument." 
 fi
